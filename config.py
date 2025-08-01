762a41ce8ccc2f8340b591edfff113f7e4a45012
niharikaequatesweb <92165893+niharikaequatesweb@users.noreply.github.com>
2025-08-01 18:32:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@
 import os
 
 OMNIDIM_API_KEY = os.getenv("OMNIDIM_API_KEY", "jVRwY6AyEWquGevctQe-OdxKBaxcD8uLtAUP1cfjd1o")
-VOICE_WIDGET_SECRET = os.getenv("VOICE_WIDGET_SECRET", "9d71809ab901a0e64cf4529da1b8c3628")
+VOICE_WIDGET_SECRET = os.getenv("VOICE_WIDGET_SECRET", "d71809ab901a0e64cf4529da1b8c3628")
 
 # For the <script> tag
 VOICE_WIDGET_SCRIPT_ID = "omnidimension-web-widget"
-VOICE_WIDGET_SCRIPT_BASE = "https://backend.omnidim.io/web_widget.js"
+VOICE_WIDGET_SCRIPT_BASE = "https://backend.omnidim.io/web_widget.js?secret_key="+VOICE_WIDGET_SECRET
