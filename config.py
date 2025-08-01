c8b0757290d1830581dacc2ec644f02733f05725
niharikaequatesweb <92165893+niharikaequatesweb@users.noreply.github.com>
2025-08-01 22:53:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,4 @@ VOICE_WIDGET_SECRET = os.getenv("VOICE_WIDGET_SECRET", "d71809ab901a0e64cf4529da
 VOICE_WIDGET_SCRIPT_ID = "omnidimension-web-widget"
 VOICE_WIDGET_SCRIPT_BASE = "https://backend.omnidim.io/web_widget.js"
 
-TOKEN='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'
-NEW_TOKEN ='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'
 JSONBIN_API_KEY = "$2a$10$3gNg8b53sjivDhK7KS/Kr.Jghx9nq5bIzyss4xLFAD/gGwa8YvXE6"
