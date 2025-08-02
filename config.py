8def09bf014d799b3ed5e1b978336e9b433dc332
niharikaequatesweb <92165893+niharikaequatesweb@users.noreply.github.com>
2025-08-02 07:15:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,3 @@ VOICE_WIDGET_SCRIPT_ID = "omnidimension-web-widget"
 VOICE_WIDGET_SCRIPT_BASE = "https://backend.omnidim.io/web_widget.js"
 
 JSONBIN_API_KEY = "$2a$10$3gNg8b53sjivDhK7KS/Kr.Jghx9nq5bIzyss4xLFAD/gGwa8YvXE6"
-PASTEBIN_API_KEY = "your_pastebin_dev_key"
-PASTEBIN_USERNAME = "your_username"
-PASTEBIN_PASSWORD = "your_password"
